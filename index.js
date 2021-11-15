d6ddc81503a85e8dd25013774543e7efb52f00f0
prachi <prachimantri1@gmail.com>
2021-11-16 01:36:04 +0530

    Add card details

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
-mongoose.connect('mongodb://localhost:27017/myDb', {
+mongoose.connect('mongodb://localhost:27017/YogaDb', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
